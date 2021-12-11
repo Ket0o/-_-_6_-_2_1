8cf016c827ce826832ed993c0defd9f01e485121
Ket0o <kupregion3@gmail.com>
2021-12-11 16:08:14 +0700

    Добавьте файлы проекта.

---
+++ Лабараторная_работа_6_задание_2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Лабараторная_работа_6_задание_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(502, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рост >190";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 368);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 443);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ФИО, рост, вес";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Лабараторная_работа_6_задание_2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Лабараторная_работа_6_задание_2
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2 = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string text = textBox1.Text;
+
+            var text1 = new List<string>();
+
+            string pattern = @"\b(\w+\s\w+[.]\w+[.])\s[-]\s(\w+\d)\s[c][m]\b";
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+            Match output = regex.Match(text);
+
+            while(output.Success)
+            {
+                if (Convert.ToInt32(output.Groups[2].Value) > 190)
+                {
+                    text1.Add(output.Groups[1].Value);
+                }
+                output = output.NextMatch();
+            }
+
+            text = string.Empty;
+
+            for (int i = 0; i < text1.Count; i++)
+            {
+                text = text + text1[i] + ' ' + ';' + ' ';
+            }
+
+
+            form2.label2.Text = text;
+
+
+            form2.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+        }
+    }
+}
